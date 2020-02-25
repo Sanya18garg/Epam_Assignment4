6405a3479a99cf149e61b5bc4fae46006d2935ef
sanya18garg <1976rajnigarg@gmail.com>
2020-02-25 21:18:35 +0530

    first commit

---
+++ src/main/java/epam_assignment4/App.java
@@ -0,0 +1,13 @@
+package epam_assignment4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
